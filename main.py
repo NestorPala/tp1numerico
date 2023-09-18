c4db0aafe504f18fed8d13cc08ba11d9eca23636
NestorPala <npalavecino@fi.uba.ar>
2023-09-18 14:31:10 -0300

    function implementations 1

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+import numpy as np
+from numpy import cos, power, sin, add, ndarray, subtract
+from numpy.linalg import norm
+
+PI = np.pi
+UPPER = 0
+LOWER = 1
+LEFT = 2
+RIGHT = 3
+
 # boundaries: [UPPER, LOWER, LEFT, RIGHT]
 def solve_discrete_laplace_sor(
         n: int,
@@ -10,23 +20,25 @@ def solve_discrete_laplace_sor(
 
 
 def spectral_radius_gs(n: int) -> float:
-    pass
+    return power(cos(PI / n), 2)
 
 
 def spectral_radius_sor(n: int) -> float:
-    pass
+    return best_w_value(n) - 1
 
 
 def best_w_value(n: int) -> float:
-    pass
+    return 2 / (1 + sin(PI / n))
 
 
 def residual(x1: list[float], x0: list[float]) -> float:
-    pass
+    arr1 = np.array(x1)
+    arr0 = np.array(x0)
+    return _2_norm(add(arr1, arr0)) / _2_norm(arr0)
 
 
-def _2_norm(x: list[float]) -> float:
-    pass
+def _2_norm(x: ndarray) -> float:
+    return norm(x, 2)
 
 
 # calcular_valor_frontera
@@ -34,18 +46,47 @@ def b_value_from_matrix_index(
         i: int,
         j: int,
         n: int,
-        boundaries: list[int]
+        boundaries: list[float]
 ) -> float:
-    pass
+    TN = boundaries[UPPER]
+    TS = boundaries[LOWER]
+    TW = boundaries[LEFT]
+    TE = boundaries[RIGHT]
+
+    if (i == 0) and (j == 0):
+        return TS + TW
+    elif (i == n - 2) and (j == 0):
+        return TN + TW
+    elif (i == 0) and (j == n - 2):
+        return TS + TE
+    elif (i == n - 2) and (j == n - 2):
+        return TN + TE
+    elif (i == 0) and (1 <= j <= n - 3):
+        return TS
+    elif (1 <= i <= n - 3) and (j == 0):
+        return TN
+    elif (1 <= i <= n - 3) and (j == n - 2):
+        return TW
+    elif (1 <= i <= n - 3) and (1 <= j <= n - 3):
+        return TE
+
+    return 0.0
 
 
 # frontera
-def boundary_values_sum(z: int) -> float:
-    pass
+def boundary_values_sum(
+        z: int,
+        n: int,
+        boundaries: list[float]
+) -> float:
+    i, j = matrix_index_from(z, n)
+    return b_value_from_matrix_index(i, j, n, boundaries)
 
 
 def matrix_index_from(z: int, n: int) -> tuple[int, int]:
-    pass
+    i = z // (n - 1)
+    j = (z % (n - 1)) - 1
+    return j, i
 
 
 # no_frontera
@@ -55,12 +96,20 @@ def internal_values_sum(
         x1: list[float],
         x0: list[float]
 ) -> float:
-    pass
+    total = 0.0
+    for adj in internal_adjacents(z, n):
+        if adj < z:
+            total += x1[adj]  # current iteration
+        else:
+            total += x0[adj]  # last iteration
+    return total
 
 
 # cota
 def error_bound(x1: list[float], x0: list[float]) -> float:
-    pass
+    arr1 = np.array(x1)
+    arr0 = np.array(x0)
+    return np.abs(subtract(arr1, arr0))
 
 
 def sor(
@@ -68,14 +117,40 @@ def sor(
         n: int,
         x1: list[float],
         x0: list[float],
-        w: float
+        w: float,
+        boundaries: list[float]
 ) -> float:
-    pass
+    b = boundary_values_sum(z, n, boundaries)
+    i = internal_values_sum(z, n, x1, x0)
+    adjacent_sum = b + i
+    z0_value = x0[z - 1]
+    z1_value = (adjacent_sum / 4 - z0_value) * w + z0_value
+    return z1_value
 
 
-def adjacents(z: int, n: int) -> list[int]:
-    pass
+# adjacents
+def internal_adjacents(z: int, n: int) -> list[int]:
+    adj = []
+    i, j = matrix_index_from(z, n)
+
+    if j-1 >= 0:
+        z_adj = z_index_from(i, j-1, n)
+        adj.append(z_adj)
+
+    if i-1 >= 0:
+        z_adj = z_index_from(i-1, j, n)
+        adj.append(z_adj)
+
+    if i+1 <= n-2:
+        z_adj = z_index_from(i+1, j, n)
+        adj.append(z_adj)
+
+    if j+1 <= n-2:
+        z_adj = z_index_from(i, j+1, n)
+        adj.append(z_adj)
+
+    return adj
 
 
-def z_index_from(i: int, j: int) -> int:
-    pass
+def z_index_from(i: int, j: int, n: int) -> int:
+    return (n - 1) * j + (i + 1)
