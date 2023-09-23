37437d6273f210e508bf54dcb1aaa9e153707bf9
NestorPala <npalavecino@fi.uba.ar>
2023-09-23 12:50:49 -0300

    added test_node_sor 2

--- tests/test_b_value_from_matrix_index.py
+++ tests/test_b_value_from_matrix_index.py
@@ -1,37 +1,49 @@
 import unittest
-from main import b_value_from_matrix_index
+from main import b_value_from_matrix_index, UPPER, LOWER, LEFT, RIGHT
 
 
 class BValueFromMatrixIndexTestCase(unittest.TestCase):
 
     def test_n_equal_to_4(self):
         n = 4
-        boundaries = [1, 1, 1, 1]
-        self.assertEqual(2, b_value_from_matrix_index(0, 0, n, boundaries))
-        self.assertEqual(1, b_value_from_matrix_index(1, 0, n, boundaries))
-        self.assertEqual(2, b_value_from_matrix_index(2, 0, n, boundaries))
+        boundaries = [-1, -1, -1, -1]
+        boundaries[UPPER] = 118
+        boundaries[LOWER] = 56.25
+        boundaries[LEFT] = -37
+        boundaries[RIGHT] = 1
+        self.assertEqual(19.25, b_value_from_matrix_index(0, 0, n, boundaries))
+        self.assertEqual(-37, b_value_from_matrix_index(1, 0, n, boundaries))
+        self.assertEqual(81, b_value_from_matrix_index(2, 0, n, boundaries))
 
-        self.assertEqual(1, b_value_from_matrix_index(0, 1, n, boundaries))
+        self.assertEqual(56.25, b_value_from_matrix_index(0, 1, n, boundaries))
         self.assertEqual(0, b_value_from_matrix_index(1, 1, n, boundaries))
-        self.assertEqual(1, b_value_from_matrix_index(2, 1, n, boundaries))
+        self.assertEqual(118, b_value_from_matrix_index(2, 1, n, boundaries))
 
-        self.assertEqual(2, b_value_from_matrix_index(0, 2, n, boundaries))
+        self.assertEqual(57.25, b_value_from_matrix_index(0, 2, n, boundaries))
         self.assertEqual(1, b_value_from_matrix_index(1, 2, n, boundaries))
-        self.assertEqual(2, b_value_from_matrix_index(2, 2, n, boundaries))
+        self.assertEqual(119, b_value_from_matrix_index(2, 2, n, boundaries))
 
     def test_n_equal_to_3(self):
         n = 3
-        boundaries = [1, 1, 1, 1]
-        self.assertEqual(2, b_value_from_matrix_index(0, 0, n, boundaries))
-        self.assertEqual(2, b_value_from_matrix_index(1, 0, n, boundaries))
+        boundaries = [-1, -1, -1, -1]
+        boundaries[UPPER] = 118
+        boundaries[LOWER] = 56.25
+        boundaries[LEFT] = -37
+        boundaries[RIGHT] = 1
+        self.assertEqual(19.25, b_value_from_matrix_index(0, 0, n, boundaries))
+        self.assertEqual(81, b_value_from_matrix_index(1, 0, n, boundaries))
 
-        self.assertEqual(2, b_value_from_matrix_index(0, 1, n, boundaries))
-        self.assertEqual(2, b_value_from_matrix_index(1, 1, n, boundaries))
+        self.assertEqual(57.25, b_value_from_matrix_index(0, 1, n, boundaries))
+        self.assertEqual(119, b_value_from_matrix_index(1, 1, n, boundaries))
 
     def test_n_equal_to_2(self):
         n = 2
-        boundaries = [1, 1, 1, 1]
-        self.assertEqual(4, b_value_from_matrix_index(0, 0, n, boundaries))
+        boundaries = [-1, -1, -1, -1]
+        boundaries[UPPER] = 118
+        boundaries[LOWER] = 56.25
+        boundaries[LEFT] = -37
+        boundaries[RIGHT] = 1
+        self.assertEqual(138.25, b_value_from_matrix_index(0, 0, n, boundaries))
 
 
 if __name__ == '__main__':
--- tests/test_node_sor.py
+++ tests/test_node_sor.py
@@ -43,6 +43,19 @@ class NodeSORTestCase(unittest.TestCase):
         boundaries[RIGHT] = 20.0
         self.assertEqual(15.8125, node_sor(z, n, x1, x0, w, boundaries))
 
+    def test_n_equal_to_4_and_z_equal_to_6(self):
+        z = 6
+        n = 4
+        x1 = [12.0, 13.0, 14.0, 15.0, 16.0]
+        x0 = [12.5, 13.5, 14.5, 15.5, 16.5, 17.5, 18.5, 19.5, 20.5]
+        w = 1.5
+        boundaries = [-1, -1, -1, -1]
+        boundaries[UPPER] = 15.0
+        boundaries[LOWER] = 15.0
+        boundaries[LEFT] = 20.0
+        boundaries[RIGHT] = 20.0
+        self.assertEqual(15.8125, node_sor(z, n, x1, x0, w, boundaries))
+
 
 if __name__ == '__main__':
     unittest.main()
