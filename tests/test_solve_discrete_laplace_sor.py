9e8bf937fa117b8966029a7cf9b07ceffce09e4a
NestorPala <npalavecino@fi.uba.ar>
2023-09-23 22:41:19 -0300

    fixed check_nodes and added more sor tests

--- tests/test_solve_discrete_laplace_sor.py
+++ tests/test_solve_discrete_laplace_sor.py
@@ -1,7 +1,19 @@
 import unittest
+from unittest import skip
+
 from main import solve_discrete_laplace_sor, UPPER, LOWER, LEFT, RIGHT
-from tests.results.test_solve_discrete_laplace_sor.PARTE_1_D_.RES_1D_W_1_446 import TEST_N_32_W_1_446_X_VALUE, TEST_N_32_W_1_446_X_ERROR_BOUND, TEST_N_32_W_1_446_RESIDUAL, TEST_N_32_W_1_446_ITERATIONS
-from tests.results.test_solve_discrete_laplace_sor.PARTE_2 import TEST_1_X_VALUE, TEST_1_X_ERROR_BOUND, TEST_1_RESIDUAL, TEST_1_ITERATIONS
+from tests.results.test_solve_discrete_laplace_sor.PARTE_1_D_.RES_1D_W_1_446 import \
+    TEST_N_32_W_1_446_X_VALUE, \
+    TEST_N_32_W_1_446_X_ERROR_BOUND, \
+    TEST_N_32_W_1_446_RESIDUAL, \
+    TEST_N_32_W_1_446_ITERATIONS
+from tests.results.test_solve_discrete_laplace_sor.PARTE_2 import \
+    TEST_1_X_VALUE, \
+    TEST_1_X_ERROR_BOUND, \
+    TEST_1_RESIDUAL, \
+    TEST_1_ITERATIONS
+from tests.utils.check_nodes import nodes_are_valid
+
 
 X_VALUE = 0
 X_ERROR_BOUND = 1
@@ -12,7 +24,6 @@ ITERATIONS = 3
 class SolveDiscreteLaplaceSORTestCase(unittest.TestCase):
 
     # Parte 2
-    # TODO: do for w values: [1.00, 1.05, 1.10, ..., 1.95]
     # Test 1
     def test_n_equals_8(self):
         n = 8
@@ -25,18 +36,20 @@ class SolveDiscreteLaplaceSORTestCase(unittest.TestCase):
         boundaries[LEFT] = 40.0
         boundaries[RIGHT] = 40.0
 
-        # w = 1.4464626921716894
+        # w = best_w_value(n)
         data = solve_discrete_laplace_sor(n, r_tol, boundaries)
 
-        x_value = data[X_VALUE]
-        x_error_bound = data[X_ERROR_BOUND]
+        x_values = data[X_VALUE]
+        x_errors = data[X_ERROR_BOUND]
         residual = data[RESIDUAL]
         iterations = data[ITERATIONS]
 
-        self.assertEqual(TEST_1_X_VALUE, x_value)
-        self.assertEqual(TEST_1_X_ERROR_BOUND, x_error_bound)
+        self.assertEqual(TEST_1_X_VALUE, x_values)
+        self.assertEqual(TEST_1_X_ERROR_BOUND, x_errors)
         self.assertEqual(TEST_1_RESIDUAL, residual)
         self.assertEqual(TEST_1_ITERATIONS, iterations)
+
+        self.assertTrue(nodes_are_valid(x_values, x_errors, n, boundaries))
 
     # c) (N = 4), d)
     def test_n_equals_32(self):
@@ -44,27 +57,78 @@ class SolveDiscreteLaplaceSORTestCase(unittest.TestCase):
         r_tol = 0.01
         boundaries = [-1, -1, -1, -1]
 
-        # 108244
         boundaries[UPPER] = 1.0
         boundaries[LOWER] = 1.0
         boundaries[LEFT] = 1.0
         boundaries[RIGHT] = 1.0
 
-        # w = 1.4464626921716894
+        # w = best_w_value(n)
         data = solve_discrete_laplace_sor(n, r_tol, boundaries)
 
-        x_value = data[X_VALUE]
-        x_error_bound = data[X_ERROR_BOUND]
+        x_values = data[X_VALUE]
+        x_errors = data[X_ERROR_BOUND]
         residual = data[RESIDUAL]
         iterations = data[ITERATIONS]
 
-        print(x_value)
-        print(x_error_bound)
-
-        self.assertEqual(TEST_N_32_W_1_446_X_VALUE, x_value)
-        self.assertEqual(TEST_N_32_W_1_446_X_ERROR_BOUND, x_error_bound)
+        self.assertEqual(TEST_N_32_W_1_446_X_VALUE, x_values)
+        self.assertEqual(TEST_N_32_W_1_446_X_ERROR_BOUND, x_errors)
         self.assertEqual(TEST_N_32_W_1_446_RESIDUAL, residual)
         self.assertEqual(TEST_N_32_W_1_446_ITERATIONS, iterations)
+
+        self.assertTrue(nodes_are_valid(x_values, x_errors, n, boundaries))
+
+    def test_n_equals_128(self):
+        n = 128
+        r_tol = 0.01
+        boundaries = [-1, -1, -1, -1]
+
+        boundaries[UPPER] = 1.0
+        boundaries[LOWER] = 1.0
+        boundaries[LEFT] = 1.0
+        boundaries[RIGHT] = 1.0
+
+        data = solve_discrete_laplace_sor(n, r_tol, boundaries)
+
+        x_values = data[X_VALUE]
+        x_errors = data[X_ERROR_BOUND]
+
+        self.assertTrue(nodes_are_valid(x_values, x_errors, n, boundaries))
+
+    @skip('(n = 256) Takes too long')
+    def test_n_equals_256(self):
+        n = 256
+        r_tol = 0.01
+        boundaries = [-1, -1, -1, -1]
+
+        boundaries[UPPER] = 1.0
+        boundaries[LOWER] = 1.0
+        boundaries[LEFT] = 1.0
+        boundaries[RIGHT] = 1.0
+
+        data = solve_discrete_laplace_sor(n, r_tol, boundaries)
+
+        x_values = data[X_VALUE]
+        x_errors = data[X_ERROR_BOUND]
+
+        self.assertTrue(nodes_are_valid(x_values, x_errors, n, boundaries))
+
+    @skip('(n = 512) Takes too long')
+    def test_n_equals_512(self):
+        n = 512
+        r_tol = 0.01
+        boundaries = [-1, -1, -1, -1]
+
+        boundaries[UPPER] = 1.0
+        boundaries[LOWER] = 1.0
+        boundaries[LEFT] = 1.0
+        boundaries[RIGHT] = 1.0
+
+        data = solve_discrete_laplace_sor(n, r_tol, boundaries)
+
+        x_values = data[X_VALUE]
+        x_errors = data[X_ERROR_BOUND]
+
+        self.assertTrue(nodes_are_valid(x_values, x_errors, n, boundaries))
 
 
 if __name__ == '__main__':
--- tests/utils/check_nodes.py
+++ tests/utils/check_nodes.py
@@ -1,16 +1,33 @@
 from main import boundary_values_sum, internal_values_sum
+from math import isclose
 
 
-def nodes_are_valid(x: list[float], n: int, boundaries: list[float]) -> bool:
-    for z in range(1, len(x) + 1):
+# Machine epsilon for 1-decimal-digit round
+U = 0.5e-01
+
+
+# Check if result of all nodes are correct
+# (approximately close each node to the sum of its adjacents)
+def nodes_are_valid(
+        x_values: list[float],
+        x_errors: list[float],
+        n: int,
+        boundaries: list[float]
+) -> bool:
+    for z in range(1, len(x_values) + 1):
         b = boundary_values_sum(z, n, boundaries)
-        i = internal_values_sum(z, n, x1=x, x0=x)
-        adjacent_sum = b + i
-        z_node_value = x[z - 1]
-        if adjacent_sum / 4 != z_node_value:
+        i = internal_values_sum(z, n, x1=x_values, x0=x_values)
+        adjacents_avg_value = (b + i) / 4
+        z_node_value = x_values[z - 1]
+        z_node_error = x_errors[z - 1]
+        if not isclose(
+                round(adjacents_avg_value, 1),
+                round(z_node_value, 1),
+                abs_tol=4 * (round(z_node_error, 1) + U)
+        ):
             return False
     return True
 
 
-assert nodes_are_valid(x=[10], n=2, boundaries=[10, 10, 10, 10])
-assert nodes_are_valid(x=[25, 22.5, 27.5, 25], n=3, boundaries=[10, 20, 30, 40])
+assert nodes_are_valid(x_values=[10], x_errors=[0.0], n=2, boundaries=[10, 10, 10, 10])
+assert nodes_are_valid(x_values=[25, 22.5, 27.5, 25], x_errors=[0.0, 0.0, 0.0, 0.0], n=3, boundaries=[10, 20, 30, 40])
