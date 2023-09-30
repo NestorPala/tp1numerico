aa7bb8079ae6feafda9b5b776542891282a61e21
NestorPala <npalavecino@fi.uba.ar>
2023-09-30 01:57:38 -0300

    added more matrices examples

--- tests/results/test_solve_discrete_laplace_sor/AAA.py
+++ tests/results/test_solve_discrete_laplace_sor/AAA.py
@@ -27,3 +27,30 @@ print(f"Los valores de temperatura de los nodos son: T =", T, "\n")
 print(f"Las cotas de error para T son: ∆T =", DELTA_T, "\n")
 print(f"El residuo final es: R =", residual, "\n")
 print(f"La cantidad de iteraciones fue: K =", iterations, "\n")
+
+# only one node
+if n == 2:
+    return TN + TS + TW + TE
+
+# node is corner
+elif (i == 0) and (j == 0):
+    return TS + TW
+elif (i == 0) and (j == n - 2):
+    return TS + TE
+elif (i == n - 2) and (j == 0):
+    return TN + TW
+elif (i == n - 2) and (j == n - 2):
+    return TN + TE
+
+# node is border
+elif (i == 0) and (1 <= j <= n - 3):
+    return TS
+elif (i == n - 2) and (1 <= j <= n - 3):
+    return TN
+elif (j == 0) and (1 <= i <= n - 3):
+    return TW
+elif (j == n - 2) and (1 <= i <= n - 3):
+    return TE
+
+# node is center
+return 0.0
